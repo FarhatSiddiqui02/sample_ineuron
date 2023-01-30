eaa1bcd4c8adde8437e1cdf667d82db247a23198
FarhatSiddiqui02 <farhatsiddiqui76448@gmail.com>
2023-01-30 15:15:07 +0530

    First comment

---
+++ demo.java
@@ -0,0 +1,4 @@
+public static void main(String s[])
+{
+   system.out.println("Hello world");
+}
---
+++ test.java
@@ -0,0 +1,7 @@
+public static void main(String s[])
+{
+   int add()
+   {
+          //some logic
+  }
+}
